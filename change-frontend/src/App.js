4e3f82ddd86e927d30677bd66ec375a2fb070977
BrandonFields <brandonefields@gmail.com>
2021-05-25 12:00:38 -0600

    state added, added components

--- change-frontend/src/App.js
+++ change-frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Goals from "./Components.js/Goals";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gittin it done
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+
+  state = {
+    goals: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/goals")
+    .then(res => res.json())
+    .then(console.log)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Form /> */}
+        <Goals />
+      </div>
+    );
+  }
 }
 
 export default App;
+
---
+++ change-frontend/src/Components.js/Goals.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Goals = () => {
+
+    const renderGoals = () => {
+        
+    }
+
+    return (
+        <div>
+            <p>Gooooallls</p>
+        </div>
+    );
+}
+
+export default Goals;
