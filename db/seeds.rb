d02c73e31884c73a78b6dfbef41c3174120253b9
Haley Warson <haleywarson@gmail.com>
2021-05-25 11:27:57 -0600

    GET and SHOW routes functioning.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Goal.destroy_all
+
+Goal.create!(
+    original_goal: "I want to feel better.",
+    original_s: "I want to feel happy.",
+    original_sm: "I want to feel like the best version of myself.",
+    original_sma: "I want to spend time doing the things that I love.",
+    original_smar: "I will set aside 1 hour each day to do something I love.",
+    start_date: 2021-05-30,
+    end_date: 2021-06-30,
+    final_goal: "Beginning on May 30th, I will meditate from 8-9am every Mon-Fri until June 30th."
+)
+
+Goal.create!(
+    original_goal: "I want to feel refreshed.",
+    original_s: "I want to be healthy.",
+    original_sm: "I want to eat healthy foods and work out.",
+    original_sma: "I will set aside time during lunch break to cook and work out.",
+    original_smar: "I will spend 30 minutes at lunch cooking healthy foods and working out.",
+    start_date: 2021-05-30,
+    end_date: 2021-06-30,
+    final_goal: "Beginning on May 30th and until June 30th, I will spend 30 minutes at lunch cooking healthy foods and doing pilates."
+)
